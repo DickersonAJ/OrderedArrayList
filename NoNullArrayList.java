595e47c5b1065f37f97a6f4f44805229dba2fb00
DickersonAJ <>
2020-12-12 15:48:26 -0500

    added 1st constructor to NoNullArrayList and tested it.

---
+++ NoNullArrayList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public NoNullArrayList(){
+    super();
+  }
+
+  /*public NoNullArrayList(int initialCapacity){
+
+  }*/
+
+  /*public boolean add(String element){
+
+  }*/
+
+  /*public String set(int index, String element){
+
+  }*/
+
+  /*public void add(int index, String element){
+
+  }*/
+
+}
---
+++ Tester.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Tester{
+
+  public static void main(String[] args){
+    NoNullArrayList<String> words = new NoNullArrayList<String>();
+    System.out.println(words);
+    NoNullArrayList<double[]> nums = new NoNullArrayList<double[]>();
+  }
+
+}
